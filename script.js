3bc82f10ce31c0c626c4fca4be49021bdebe6547
Olya2125 <139589012+Olya2125@users.noreply.github.com>
2024-07-04 22:17:35 +0300

    feat: add animation, chahges color, add block Code Example

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const letters = ['web developer and', 'web designer']
+
+gsap.to(".line" , {
+    opacity: 0,
+    repeat: -1,
+    yoyo: true,
+    duration: 0.5,
+    ease: "power2.inOut"
+});
+
+let time = gsap.timeline({
+    repeat: -1
+});
+
+letters.forEach((word) => {
+    let textAll = gsap.timeline({
+        repeat: 1,
+        yoyo: true,
+        repeatDelay: 1
+    });
+    textAll.to(".animation-first", {
+        duration: 1.6,
+        text: word,
+    });
+    time.add(textAll);
+})
+
+
 const moreContent = document.getElementById("moreContent");
 const content = document.getElementById("content")
 
@@ -10,4 +38,12 @@ moreContent.addEventListener('click', function() {
         }
 } )
 
+
+let menuBurger = document.querySelector("#menu-burger");
+let changes = document.querySelector("#menu");
+
+menuBurger.addEventListener("click", function() {
+    this.classList.toggle("close");
+    changes.classList.toggle("changes");
+})
 
